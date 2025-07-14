d3b266926025962bba5163e8b45e3d0e129f7f76
Mailson Teles Borges <mailson.borges@sou.unijui.edu.br>
2025-07-14 15:00:59 -0300

    Update the smart contract implementation

--- oliveira-rodrigues/pbb.sol
+++ oliveira-rodrigues/pbb.sol
@@ -1,26 +1,34 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30;
+pragma solidity 0.8.30;
 
 contract FairExchangePbb {
     enum Status { Accepted, Rejected, Undefined }
     struct Party {
         address account;
+        string token;
         Status status;
     }
 
     Party public partyA;
     Party public partyB;
 
-    constructor(address _partyA, address _partyB) {
-        partyA = Party({
-            account: _partyA,
-            status: Status.Undefined
-        });
+    event ExchangeAccepted(address indexed party, string token);
+    event ExchangeRejected(address indexed party, string token);
+    event ExchangeCompleted(address indexed partyA, address indexed partyB, string tokenA, string tokenB);
 
-        partyB = Party({
-            account: _partyB,
-            status: Status.Undefined
-        });
+    constructor(address accountA, address accountB) payable {
+        require(msg.value == 0, "No ETH allowed");
+        require(accountA != address(0), "A: zero address");
+        require(accountB != address(0), "B: zero address");
+        require(accountA != accountB, "Same addresses");
+
+        partyA.account = accountA;
+        partyA.token = "";
+        partyA.status = Status.Undefined;
+
+        partyB.account = accountB;
+        partyB.token = "";
+        partyB.status = Status.Undefined;
     }
 
     function isParty(address _addr) public view returns (bool) {
@@ -28,41 +36,63 @@ contract FairExchangePbb {
     }
 
     modifier onlyParty() {
-        require(isParty(msg.sender), "Only parties can call this");
+        require(isParty(msg.sender), "Not a party");
         _;
     }
 
-    modifier notRejected() {
-        require(partyA.status != Status.Rejected && partyB.status != Status.Rejected, "The exchange was rejected");
+    modifier isRejected() {
+        require(partyA.status != Status.Rejected && partyB.status != Status.Rejected, "Already rejected");
         _;
     }
 
-    modifier accepted() {
-        require(!(partyA.status == Status.Accepted && partyB.status == Status.Accepted), "Both parties have accepted the exchange");
+    modifier isAccepted() {
+        require(!(partyA.status == Status.Accepted && partyB.status == Status.Accepted), "Already accepted");
         _;
     }
 
-    function accept() external onlyParty notRejected accepted {
-        if (msg.sender == partyA.account) {
-            partyA.status = Status.Accepted;
+    function accept(string memory token) external onlyParty isRejected isAccepted {
+        address sender = msg.sender;
+
+        Party storage pa = partyA;
+        Party storage pb = partyB;
+
+        if (sender == pa.account && pa.status != Status.Accepted) {
+            pa.status = Status.Accepted;
+            pa.token = token;
+            emit ExchangeAccepted(sender, token);
         }
 
-        if (msg.sender == partyB.account) {
-            partyB.status = Status.Accepted;
+        if (sender == pb.account && pb.status != Status.Accepted) {
+            pb.status = Status.Accepted;
+            pb.token = token;
+            emit ExchangeAccepted(sender, token);
+        }
+
+        if (pa.status == Status.Accepted && pb.status == Status.Accepted) {
+            emit ExchangeCompleted(pa.account, pb.account, pa.token, pb.token);
         }
     }
 
-    function reject() external onlyParty notRejected accepted {
-        if (msg.sender == partyA.account) {
-            partyA.status = Status.Rejected;
+    function reject(string memory token) external onlyParty isRejected isAccepted {
+        address sender = msg.sender;
+
+        Party storage pa = partyA;
+        Party storage pb = partyB;
+
+        if (sender == pa.account && pa.status != Status.Rejected) {
+            pa.status = Status.Rejected;
+            pa.token = token;
+            emit ExchangeRejected(sender, token);
         }
 
-        if (msg.sender == partyB.account) {
-            partyB.status = Status.Rejected;
+        if (sender == pb.account && pb.status != Status.Rejected) {
+            pb.status = Status.Rejected;
+            pb.token = token;
+            emit ExchangeRejected(sender, token);
         }
     }
 
-    function release() public onlyParty view returns (bool) {
-        return (partyA.status == Status.Accepted) && (partyB.status == Status.Accepted);
+    function release() public onlyParty view returns (Party memory, Party memory) {
+        return (partyA, partyB);
     }
 }
